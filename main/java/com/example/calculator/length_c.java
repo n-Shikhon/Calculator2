730d84a921487b1ccd1b28ce84bf3ba66370da1e
sanjanatishan34 <55355747+sanjanatishan34@users.noreply.github.com>
2019-09-16 11:39:45 +0600

    Update length_c.java

--- main/java/com/example/calculator/length_c.java
+++ main/java/com/example/calculator/length_c.java
@@ -3,12 +3,46 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
 
-public class length_c extends AppCompatActivity {
+
+public class temperature extends AppCompatActivity {
+
+    private Button btncenTom,btnmTocen;
+    private TextView result;
+    private EditText enterlen;
+    double result00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_length_c);
+
+        btncenTom = findViewById(R.id.btncenTom);
+        btnfmTocen = findViewById(R.id.btnmTocen);
+        result  = findViewById(R.id.result);
+        enterlen = findViewById(R.id.enterlen);
+
+        btncenTom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double length = Double.parseDouble(enterlen.getText().toString());
+                result00 = (length /1000);
+                result.setText(String.valueOf(result00));
+            }
+        });
+
+        btnmTocen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double length = Double.parseDouble(enterlen.getText().toString());
+                result00 = (length*100);
+                result.setText(String.valueOf(result00));
+            }
+        });
+        }
+
+
     }
 }
