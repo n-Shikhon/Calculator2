0b693ae6a2e434b15d6f71eb6d4352c67f5b6e17
parisa-nawal <55357829+parisa-nawal@users.noreply.github.com>
2019-09-16 12:06:11 +0600

    Update length_f.java

--- main/java/com/example/calculator/length_f.java
+++ main/java/com/example/calculator/length_f.java
@@ -3,12 +3,46 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
 
-public class length_f extends AppCompatActivity {
+
+public class temperature extends AppCompatActivity {
+
+    private Button btnftoi,btnmitof;
+    private TextView result_f;
+    private EditText enterlen_f;
+    double result00f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_length_f);
+
+        btnftoi = findViewById(R.id.btnftoi);
+        btnitof = findViewById(R.id.btnitof);
+        result_f  = findViewById(R.id.result_f);
+        enterlen_f = findViewById(R.id.enterlen_f);
+
+        btnftoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double length_f = Double.parseDouble(enterlen_f.getText().toString());
+                result00f = (length_f*12);
+                result_f.setText(String.valueOf(result00f));
+            }
+        });
+
+        btnitof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double length_f = Double.parseDouble(enterlen_f.getText().toString());
+                result00f = (length_f/12);
+                result_f.setText(String.valueOf(result00f));
+            }
+        });
+        }
+
+
     }
 }
